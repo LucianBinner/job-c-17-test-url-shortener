304d2e565b43c16db1c1d3e23f8fbbdeac7a52cb
Lucian Binner <lucian.binner@hotmail.com>
2024-08-31 16:30:10 -0300

    chore: add jest configuration and other lint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,11 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
   extends: [
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   root: true,
